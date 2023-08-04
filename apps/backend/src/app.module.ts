905981306b86bdcb0452612c588763393e276e11
Ben Zaad <ben@octup.com>
2023-08-04 11:12:35 +0300

    connect mysql db

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,13 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ServeStaticModule} from "@nestjs/serve-static";
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import {join} from "path";
+import {DataSource} from "typeorm";
 
 @Module({
   imports: [ServeStaticModule.forRoot({
     rootPath: join(__dirname, '../..', 'frontend','dist')
+  }),
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'password',
+    database: 'test',
+    entities: [],
+    synchronize: true,
   })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
